af176726b2f58567a2163e8f40ae7f332fc2da78
Harriet-code <harriosas@gmail.com>
2021-07-28 19:57:04 +0100

    update

--- assets/js/harmburger.js
+++ assets/js/harmburger.js
@@ -1,7 +1,14 @@
 // DECLARING AND GETTING THE CONST ID WE WANT TO PUT FUNCTIONS TO. FIRST YOU GIVE THE DIV CLASS AN ID OR A CLASS YOU WANT TO TARGET
-const navItemContainer = document.getElementById("nav-item-wrapper");
+// const navItemContainer = document.getElementById("nav-item-wrapper");
 
 // FUNCTION THAT OPENS AND CLOSES THE NAV-BAR
-function toggleNavbar() {
-  navItemContainer.classList.toggle("open-nav-item");
-}
+// function toggleNavbar() {
+//   navItemContainer.classList.toggle("open-nav-item");
+// }
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
